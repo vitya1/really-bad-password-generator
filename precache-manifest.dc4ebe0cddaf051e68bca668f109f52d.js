efd488859406ebfecb286488ab42b4154c878f12
vitya1 <victor.sheremetov@gmail.com>
2019-04-23 17:53:04 +0300

    Updates

---
+++ precache-manifest.dc4ebe0cddaf051e68bca668f109f52d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc0da72556329b52e26",
+    "url": "/really-bad-password-generator/static/css/main.be2a2522.chunk.css"
+  },
+  {
+    "revision": "efc0da72556329b52e26",
+    "url": "/really-bad-password-generator/static/js/main.efc0da72.chunk.js"
+  },
+  {
+    "revision": "c9e2d98a85b2ef5a7161",
+    "url": "/really-bad-password-generator/static/css/1.322e0026.chunk.css"
+  },
+  {
+    "revision": "c9e2d98a85b2ef5a7161",
+    "url": "/really-bad-password-generator/static/js/1.c9e2d98a.chunk.js"
+  },
+  {
+    "revision": "25d390194192c7f79473",
+    "url": "/really-bad-password-generator/static/js/runtime~main.25d39019.js"
+  },
+  {
+    "revision": "4405411a476593fef622fd37cf87814d",
+    "url": "/really-bad-password-generator/static/media/logo.4405411a.png"
+  },
+  {
+    "revision": "6c4ef9b44a616ebad697eafe8a371ea3",
+    "url": "/really-bad-password-generator/index.html"
+  }
+];
